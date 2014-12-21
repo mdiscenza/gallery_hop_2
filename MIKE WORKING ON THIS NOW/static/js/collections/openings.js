15816419898949c565eee8cae944629ada704caf
Michael Discenza <michael.discenza@gmail.com>
2014-12-21 10:45:27 -0500

    created and fetched events collection from flask

--- MIKE WORKING ON THIS NOW/static/js/collections/openings.js
+++ MIKE WORKING ON THIS NOW/static/js/collections/openings.js
@@ -14,11 +14,33 @@ var app = app || {};
     //     console.log("parse called")
     //     return response.collection;
     // }
+    render: function(){
+      this.collection.each(function(opening){
+          var openingView = new app.OpeningView({ model: opening });
+          this.$el.append(openingView.el);
+      }, this);
+  }
 
 
   });
 
   // Create our global collection of **Todos**.
   app.openings = new Openings();
-  console.log(app.openings.fetch());
+  // console.log(app.openings.fetch());
+  // console.log(app.openings);
+
+  app.openings.fetch({
+  success : function(collection, response) {
+    console.log(response);
+    response.each( function (opening){
+      console.log(opening)
+      var openingView = new app.OpeningView({ model: opening });
+    })
+  },
+
+  error : function(collection, response) {
+    // code here
+  }
+  });
+
   })();
--- MIKE WORKING ON THIS NOW/static/js/views/opening-view.js
+++ MIKE WORKING ON THIS NOW/static/js/views/opening-view.js
@@ -4,39 +4,26 @@ var app = app || {};
 (function ($) {
   'use strict';
 
-  // Todo Item View
-  // --------------
-
-  // The DOM element for a todo item...
-  app.Opening = Backbone.View.extend({
+  
+  app.OpeningView = Backbone.View.extend({
     //... is a list tag.
     tagName:  'div',
 
-    // Cache the template function for a single item.
-    template: _.template($('#opening-template').html()),
+  template: _.template( $('#personTemplate').html()),
 
-    // The DOM events specific to an item.
-    events: {
-      // 'click .toggle': 'toggleCompleted',
-      // 'dblclick label': 'edit',
-      // 'click .destroy': 'clear',
-      // 'keypress .edit': 'updateOnEnter',
-      // 'keydown .edit': 'revertOnEscape',
-      // 'blur .edit': 'close'
-    },
+  initialize: function(){
+      this.render();
+  },
 
-    // The TodoView listens for changes to its model, re-rendering. Since there's
-    // a one-to-one correspondence between a **Todo** and a **TodoView** in this
-    // app, we set a direct reference on the model for convenience.
-    initialize: function () {
-      console.log("opening view created");
-    },
+  render: function(){
+      console.log("rendering opening template")
+      this.$el.html( this.template(this.model.toJSON()));
+  }
 
-    // Re-render the titles of the todo item.
-    render: function () {
 
-      this.$input = this.$('.edit');
-      return this;
-    },
+
+
+
+
   });
 })(jQuery);
