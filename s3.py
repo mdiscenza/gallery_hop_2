8f32e19baeb07359ea9547268ad86795e304e728
skg2122 <skg2122@columbia.edu>
2014-12-21 18:15:55 -0500

    Change

--- s3.py
+++ s3.py
@@ -8,8 +8,10 @@ url_list = []
 
 for f in files:
 	curr = open(f)
+	try:
 		conn.upload(f,curr,'galleryhop')
-
+	catch:
+		files.remove(f)
 for f in files:
 	curr = 'https://s3.amazonaws.com/galleryhop/'+f
 	url_list.append(curr)
