66c2c40ce7d70ef5e8bc456b28a877db0508bd35
Michael Discenza <michael.discenza@gmail.com>
2014-12-22 22:40:03 -0500

    new table source, logic for update

--- make_table.py
+++ make_table.py
@@ -7,23 +7,25 @@ cnx = mysql.connector.connect(user='galleryhop', password='galleryhop', host='ga
 
 cursor = cnx.cursor()
 
-#t = """CREATE TABLE galleries(
-#shows	VARCHAR(50),
-#date	VARCHAR(50),
-#start	VARCHAR(50),
-#end	VARCHAR(50),
-#gallery	VARCHAR(50),
-#address	VARCHAR(50),
-#nbhood	VARCHAR(50),
-#e_date	VARCHAR(50))"""
+t = """CREATE TABLE galleries_formatted(
+shows	VARCHAR(50),
+start_date	VARCHAR(50),
+start	VARCHAR(50),
+end	VARCHAR(50),
+gallery	VARCHAR(50),
+address	VARCHAR(50),
+nbhood	VARCHAR(50),
+end_date	VARCHAR(50))"""
 
-#cursor.execute(t)
-#cnx.commit()
+cursor.execute(t)
+cnx.commit()
 
-f = open('Data.csv','rU')
+f = open('data/galleries_table_date_formated.csv','rU') 
 csv_f = csv.reader(f)
 
 for row in csv_f:
-	stmt = "INSERT INTO galleries (shows,date,start,end,gallery,address,nbhood,e_date) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)"
+	stmt = "INSERT INTO galleries_formatted (shows,start_date,start,end,gallery,address,nbhood,end_date) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)"
 	cursor.execute(stmt,row)
 	cnx.commit()
+
+
